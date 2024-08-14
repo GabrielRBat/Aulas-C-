09230a1d3aeba45d06af61652951e19fc49df1a2
Gabriel Batista <gabriel.r.batista@outlook.com>
2024-08-13 21:08:03 -0300

    Commit secundári (Aprendizado de sintaxe)

--- Program.cs
+++ Program.cs
@@ -2,8 +2,26 @@
 
 int a = 10;
 int b = 5;
+long c = 100;
+float d = 1.1f;
+double e = 1.1;
 
-int r =  a + b;
+var x = 100;
+var str = "Tmj lek q isso";
 
-Console.WriteLine("Result: " + r);
+Console.WriteLine("Digite a nota 1: ");
+var nota1 = Console.ReadLine();
 
+Console.WriteLine("Digite a nota 2: ");
+var nota2 = Console.ReadLine();
+
+double mediaArt = (Convert.ToDouble(nota1) + Convert.ToDouble(nota2)) / 2;
+
+if(mediaArt >= 6)
+{
+    Console.WriteLine("Nota total: " + mediaArt);
+    Console.WriteLine("Aluno aprovado!");
+}
+
+Console.WriteLine("Nota total: " + mediaArt);
+Console.WriteLine("Aluno reprovado! L");
